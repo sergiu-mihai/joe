b5284f2b789580a360aa78a52fa81256e0551f6b
sergiu-mihai <smcojocaru@yahoo.com>
2016-03-29 21:51:37 +0100

    add otp application

---
+++ src/joe.erl
@@ -0,0 +1,6 @@
+-module(joe).
+
+-export([start/0]).
+
+start() ->
+	io:format("Hello my name is Joe~n").
---
+++ src/joe_app.erl
@@ -0,0 +1,16 @@
+-module(joe_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    joe_sup:start_link().
+
+stop(_State) ->
+    ok.
