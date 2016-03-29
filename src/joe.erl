681cd92260537d341c71f1f83c06aaa0815e01ec
sergiu-mihai <smcojocaru@yahoo.com>
2016-03-29 23:04:38 +0100

    joe updated with bitcask

--- src/joe.erl
+++ src/joe.erl
@@ -3,4 +3,18 @@
 -export([start/0]).
 
 start() ->
-	io:format("Hello my name is Joe~n").
+	Handle = bitcask:open("joe_database", [read_write]),
+	N = fetch(Handle),
+	store(Handle, N+1),
+	io:format("Joe has been run ~p times~n", [N]),
+	bitcask:close(Handle),
+	init:stop().
+	
+store(Handle, N) ->
+	bitcask:put(Handle, <<"joe_executions">>, term_to_binary(N)).
+	
+fetch(Handle) ->
+	case bitcask:get(Handle, <<"joe_executions">>) of
+		not_found -> 1;
+		{ok, Bin} -> binary_to_term(Bin)
+	end.
